f54bc320f18d2a63b8e6948fb6dd809e5d948b0f
Howard Zhang <75195784+Howardzhangdqs@users.noreply.github.com>
2022-05-26 09:42:52 +0800

    Add files via upload

--- Quickly.js
+++ Quickly.js
@@ -1,7 +1,9 @@
 const screenshot = require("desktop-screenshot");
+const tesseract  = require('node-tesseract');
 const getPixels  = require("get-pixels");
-const chalk      = require("chalk");
 const robot      = require("robotjs");
+const chalk      = require("chalk");
+const jimp       = require('jimp');
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 /* 自定义（请根据您的国象联盟软件自行修改） */
@@ -10,10 +12,13 @@ const Theme_color = [113, 134, 184];     // 主题色（默认[113, 134, 184]）
 const Fault_Tolerant = 2;                // 容错
 const Judge_factor = 300;                // 棋盘的大致长宽（用于判定棋盘位置，需要保证小于真实大小）
 const Robot_factor = 0.8;                // Robotjs的缩放参数（请各位按照自己的Robotjs调一下）
-const Time_interval = 300;               // 每次点击的间隔时间（单位: 毫秒）
-const Time_Interval = 0;               // 每轮点击之间的间隔时间（单位: 毫秒）
+const Time_interval = 400;               // 每次点击的间隔时间（单位: 毫秒）
+const Time_Interval = 100;               // 每轮点击之间的间隔时间（单位: 毫秒）
 
 const img_path = "screenshot.png";
+const img_temp = "screenshot_temp.png";
+
+const tess_options = { psm: "10 digits" };	// Tesseract参数，可根据喜好更改
 
 /* 程序部分（基于BUG运行，非专业人士请勿乱改） */
 
@@ -21,6 +26,8 @@ var cb;                    // 棋盘大小
 var pic;                // 屏幕截图
 var cb_content = [];    // 棋盘内容
 var pend;
+var round_f;
+var delay = 1000;
 
 var vis = [], tot, ans = [];
 var dx = [0, 1, 1, 2, 2, -1, -1, -2, -2];
@@ -92,50 +99,87 @@ var trans_ = function(x, y) {
     return [Math.round(cb[0] - cb[2] / 16 + cb[2] / 8 * x), Math.round(cb[1] - cb[2] / 16 + cb[2] / 8 * y)];
 }
 
-// 获取格子内容
-var get_block = function(x, y) {
-    let t = trans_(x, y);
-    let r = pic.get(t[0], t[1], 0), g = pic.get(t[0], t[1], 1), b = pic.get(t[0], t[1], 2);
-    if (check_iscolor([r, g, b], [0, 0, 0])) return "P";
-    for (let i = 1; i <= cb[2] / 16 - 2; i ++) {
-        r = pic.get(t[0] + i, t[1], 0), g = pic.get(t[0] + i, t[1], 1), b = pic.get(t[0] + i, t[1], 2);
-        if (r + g + b < 80) return "K";
-    }
-    return ".";
+var trans_p2n = async function(t) {
+	t = t.trim();
+	if (t.length == 1 && t.charCodeAt() >= 49 && t.charCodeAt() <= 56) t = "f" + t;
+	if ((t.trim()).length == 2 && !/^[A-Za-z]+$/.test(t)) {
+		let tn = 57 - t[1].charCodeAt(), tp = t[0].charCodeAt() - 96;
+		let tt = trans_(tp, tn);
+		robot.moveMouse(tt[0] * Robot_factor, tt[1] * Robot_factor);
+		robot.mouseClick();
+		await wait(delay);
+		if (delay >= 10) delay -= 10;
+		console.log("点击成功");
+	}
+	//await wait(500);
+	await screenshot(img_path, (async (error, complete) => {
+        console.log();
+        if (error) console.log(chalk.red("屏幕截图失败"), error);
+        else {
+            console.log(chalk.green("屏幕截图成功"));
+			get_checkerboard_();
+        }
+    }));
 }
 
-var run_robot = async function() {
-    await wait(ans.length * Time_Interval);
-    for (let i in ans) {
-        let t = trans_(ans[i][0], ans[i][1]);
-        robot.moveMouse(t[0] * Robot_factor, t[1] * Robot_factor);
-        robot.mouseToggle("down");await wait(10);
-        robot.mouseToggle("up");
-        pend = [ans[i][0], ans[i][1]];
-        await wait(Time_interval);
+// 获取格子内容
+var get_block = async function(x, y) {
+	//await wait(10000);
+    let t = trans_(x, y);
+    for (let i = 1; i <= cb[2] / 32 && ! round_f; i ++) {
+        r = pic.get(t[0] + i, t[1], 0), g = pic.get(t[0] + i, t[1], 1), b = pic.get(t[0] + i, t[1], 2);
+        if (check_iscolor([r, g, b], Theme_color)) {
+		console.log(r, g, b);
+			round_f = true;
+			console.log(x, y);
+			console.log(t[0] + i, t[1]);
+			await jimp.read(img_path, async function (err, img) {
+				if (err) console.log(chalk.red("识别错误"), err);
+				else {
+					await img.crop(t[0] - cb[2] / 16 + 2, t[1] - cb[2] / 16 + 2, cb[2] / 8 - 4, cb[2] / 8 - 4).write(img_temp);
+					console.log("切图成功");
+					await tesseract.process(img_temp, tess_options, async (err, res) => {
+						if (err) {
+							console.log(chalk.red("识别错误"), err);
+						} else {
+							console.log("OCR 识别出: " + res);
+							trans_p2n(res);
+						}
+					});
+				}
+			})
+		}
     }
-    await wait(500); // 五百毫秒延时等待国象联盟下一轮加载完毕
-    main();
 }
 
 // 获取棋盘内容
-var get_checkerboard_ = function() {
-    tot = 0;
-    let start;
-    ans = [];
-    for (let x = 1; x <= 8; x ++)
-        for (let y = 1; y <= 8; y ++) {
-            cb_content[x][y] = get_block(x, y);
-            if (cb_content[x][y] == "P") tot ++;
-            if (cb_content[x][y] == "K") start = [x, y];
+var get_checkerboard_ = async function() {
+	await getPixels(img_path, async function(err, pixels) {
+        if (err) {
+            console.log("Bad image path"); return;
+        } else {
+            pic = pixels;
+			tot = 0;
+			let start;
+			ans = [];
+			//await wait(1000);
+			console.log("重开"); round_f = false;
+			for (let x = Math.floor(Math.random() * 1000) % 8 + 1, i = 1; i <= 8 && ! round_f; x = Math.floor(Math.random() * 1000) % 8 + 1, i ++)
+				for (let y = 1; y <= 8 && ! round_f; y ++) {
+					//console.log(x, y);
+					if ((x + y) % 2 == 1) continue;
+					await get_block(x, y);
+				}
+			await screenshot(img_path, (async (error, complete) => {
+				console.log();
+				if (error) console.log(chalk.red("屏幕截图失败"), error);
+				else {
+					console.log(chalk.green("屏幕截图成功"));
+					get_checkerboard_();
+				}
+			}));
         }
-    print_checkerboard();
-    //if (start == undefined) start = pend;
-    dfs(start[0], start[1], tot);
-	console.log("路径计算完毕");
-    print_checkerboard_();
-    //console.log(ans);
-    run_robot();
+    })
 }
 
 var get_checkerboard = (async () => {
@@ -171,10 +215,6 @@ var get_checkerboard = (async () => {
 });
 
 let main = (async () => {
-    cb_content = [], tot = 0, vis = [], ans = [];
-    for (let i = 1; i <= 9; i ++) cb_content.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    for (let i = 1; i <= 9; i ++) vis.push([false, false, false, false, false, false, false, false, false]);
-    
     await screenshot(img_path, (async (error, complete) => {
         console.log();
         if (error) console.log(chalk.red("屏幕截图失败"), error);
